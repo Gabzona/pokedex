84aef04ffea5a0acae17828afa478b2ed03e7739
GABRIELLA FRANCISCO <gabriella_francisco@estudante.sesisenai.org.br>
2025-05-21 08:45:00 -0300

    começando o javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+//referenciar os elementos
+
+const botaoBuscar = document.getElementById("botao-buscar")
+const campoEntrada = document.getElementById("entrada")
+
+//evento ao clicar, fazer a requisição
+botaoBuscar.addEventListener("click", async () => {
+    const busca = campoEntrada.value.toLowerCase().trim();
+    if(!busca) return; //se estiver vazio
+    try{
+        //faz a req.
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`)
+        if(!resposta.ok) throw new Error("Pokémon não encontrado")
+
+        //se ok, converte em JSON
+        const dados = await resposta.json()
+
+        //atualizar os dados
+        document.getElementById("nome").textContent = dados.name;
+        document.getElementById("numero").textContent = `${dados.id}`;
+        document.getElementById("imagem").src = dados.sprites.front_default;
+    } catch(erro){
+        alert(erro.message);
+    }
+});
